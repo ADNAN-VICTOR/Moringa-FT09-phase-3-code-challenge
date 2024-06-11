f69d998711e5b90d915a252241a73774faa3ef3e
adnan <adnan.victor@student.moringaschool.com>
2024-06-11 10:45:50 +0300

    edit

--- models/author.py
+++ models/author.py
@@ -4,14 +4,14 @@ class Author:
     def __init__(self, id, name):
         self.id = id
         self.name = name
-    
+
     def articles(self):
         query = """
             SELECT * 
             FROM articles 
             WHERE author_id = ?
         """
-        with get_db_connection() as conn:
+        with get_db_connection.get_db_connection() as conn:
             cursor = conn.cursor()
             cursor.execute(query, (self.id,))
             return cursor.fetchall()
@@ -23,7 +23,7 @@ class Author:
             JOIN articles ON magazines.id = articles.magazine_id 
             WHERE articles.author_id = ?
         """
-        with get_db_connection() as conn:
+        with get_db_connection.get_db_connection() as conn:
             cursor = conn.cursor()
             cursor.execute(query, (self.id,))
             return cursor.fetchall()
